df5ac4310dc0243d7a03784123ddc6d759114d28
Charles Oliveira <charlesbritoneresjr@gmail.com>
2026-05-06 15:16:17 +0800

    feat: add navigation links

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,5 +1,22 @@
+import { Link, Outlet } from "react-router-dom";
 function Clients() {
-  return <h1>Clients</h1>;
+  return (
+    <div>
+      <header>
+        <h2>ClientFlow</h2>
+
+        <nav>
+          <Link to="/">Dashboard</Link>
+          <Link to="/clients">Clients</Link>
+          <Link to="/tasks">Tasks</Link>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default Clients;
